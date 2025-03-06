2ed8e94b310af63ab94107cad86704a7f1c02fda
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-03-06 16:43:00 +0200

    in task 2 added bool check

--- index.py
+++ index.py
@@ -26,8 +26,12 @@ def get_days_from_today(date=None):
 
 def get_numbers_ticket(min=None, max=None, quantity=None):
   if min is None or max is None or quantity is None:
-    return 'You should pass min,max and quantity for this function to work'
+    return []
   
+  for arg in (min, max, quantity):  #to throw away bools
+    if isinstance(arg, bool):
+      return []
+
   try:
     if  1 <= min < max and min < max <= 1000 and 1 <= quantity <= 1000:
       result = set()
@@ -36,8 +40,9 @@ def get_numbers_ticket(min=None, max=None, quantity=None):
       return sorted(list(result))
     else: 
       return []
-  except (ValueError, TypeError):
-    return 'You did not provide correct type of value'
+  except (ValueError, TypeError) as error:
+    print(error) #do I need to print error? 
+    return []
 
 # print(get_numbers_ticket())
 # print(get_numbers_ticket(2,36,7)) #correct
@@ -46,7 +51,7 @@ def get_numbers_ticket(min=None, max=None, quantity=None):
 # print(get_numbers_ticket(1001,1000,3)) 
 # print(get_numbers_ticket(1,1,3)) 
 # print(get_numbers_ticket(True, '1000', 32))
-# print(get_numbers_ticket(1, 32, True))
+# print(get_numbers_ticket(1, 32, True)) 
 # print(get_numbers_ticket([], [],[]))
 
 
